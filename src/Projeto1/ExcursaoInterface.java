15a12c873455ff9761eb9bda3a2b922b4b8e41a0
Luiz Henrique <89359053+LuizHenriqueGit@users.noreply.github.com>
2023-10-01 16:03:50 -0300

    Versao final (atualizações)
    Melhorado o tamanho da fonte das letras e a cor da letra da área de texto.

--- src/Projeto1/ExcursaoInterface.java
+++ src/Projeto1/ExcursaoInterface.java
@@ -74,7 +74,7 @@ public class ExcursaoInterface {
 		//area de texto
 		JTextArea txtrAsReservasDa = new JTextArea();
 		txtrAsReservasDa.setLineWrap(true);
-		txtrAsReservasDa.setFont(new Font("Gadugi", Font.PLAIN, 11));
+		txtrAsReservasDa.setFont(new Font("Gadugi", Font.PLAIN, 13));
 		txtrAsReservasDa.setText("Os dados da excursão aparecerão aqui.");
 		txtrAsReservasDa.setBounds(170, 74, 200, 302);
 		txtrAsReservasDa.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.BLACK, 1), BorderFactory.createEmptyBorder(5, 5, 5, 5)));
@@ -90,46 +90,46 @@ public class ExcursaoInterface {
 		    @Override
 		    public void focusLost(FocusEvent e) {
 		        if (txtrAsReservasDa.getText().isEmpty()) {
-		        	txtrAsReservasDa.setForeground(Color.GRAY);
+		        	txtrAsReservasDa.setForeground(Color.BLACK);
 		        	txtrAsReservasDa.setText("Os dados da excursão aparecerão aqui.");
 		        }
 		    }
 		    });
 		frameExcursao.getContentPane().add(txtrAsReservasDa);
 		
-		//título
+		//titulo
 		JLabel lblNewLabel = new JLabel("Projeto 1 - Excursão");
 		lblNewLabel.setFont(new Font("Gadugi", Font.BOLD, 19));
 		lblNewLabel.setBounds(102, 11, 180, 23);
 		frameExcursao.getContentPane().add(lblNewLabel);
 		
-		//subtítulo
+		//subtitulo
 		JLabel lblNewLabel_3 = new JLabel("Grupo: Luiz Henrique e Lucas Jaud");
 		lblNewLabel_3.setBounds(102, 34, 205, 14);
 		lblNewLabel_3.setFont(new Font("Gadugi", Font.PLAIN, 11));
 		frameExcursao.getContentPane().add(lblNewLabel_3);
 		
-		//subtítulo 1
+		//subtitulo 1
 		JLabel lblNewLabel_1 = new JLabel("Reservas");
 		lblNewLabel_1.setFont(new Font("Gadugi", Font.BOLD, 13));
 		lblNewLabel_1.setBounds(59, 192, 63, 14);
 		frameExcursao.getContentPane().add(lblNewLabel_1);
 		
-		//subtítulo 2
+		//subtitulo 2
 		JLabel lblNewLabel_2 = new JLabel("Excursão");
 		lblNewLabel_2.setFont(new Font("Gadugi", Font.BOLD, 13));
 		lblNewLabel_2.setBounds(59, 77, 63, 14);
 		frameExcursao.getContentPane().add(lblNewLabel_2);
 		
-		//versão
+		//versao
 		JLabel label = new JLabel("V1.0");
 		label.setFont(new Font("Gadugi", Font.BOLD, 12));
 		label.setBounds(73, 396, 46, 14);
 		frameExcursao.getContentPane().add(label);
 		
-		//botão: criar reserva
+		//botao: criar reserva
 		JButton btnCriarRes = new JButton("Criar reserva");
-		btnCriarRes.setFont(new Font("Gadugi", Font.PLAIN, 11));
+		btnCriarRes.setFont(new Font("Gadugi", Font.PLAIN, 12));
 		btnCriarRes.setEnabled(false);
 		btnCriarRes.setBounds(29, 217, 120, 23);
 		btnCriarRes.addActionListener(new ActionListener() {
@@ -145,11 +145,11 @@ public class ExcursaoInterface {
 		});
 		frameExcursao.getContentPane().add(btnCriarRes);
 		
-		//botão: cancelar reserva
-		JButton btnCancelarRes = new JButton("Cancelar (Nome)");
+		//botao: cancelar reserva
+		JButton btnCancelarRes = new JButton("Cancelar(Nome)");
 		btnCancelarRes.setBounds(29, 285, 120, 23);
 		btnCancelarRes.setEnabled(false);
-		btnCancelarRes.setFont(new Font("Gadugi", Font.PLAIN, 11));
+		btnCancelarRes.setFont(new Font("Gadugi", Font.PLAIN, 12));
 		btnCancelarRes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -163,9 +163,9 @@ public class ExcursaoInterface {
 		});
 		frameExcursao.getContentPane().add(btnCancelarRes);
 		
-		//botão: cancelar cpf
-		JButton btnCancelarRes_1 = new JButton("Cancelar (CPF)");
-		btnCancelarRes_1.setFont(new Font("Gadugi", Font.PLAIN, 11));
+		//botao: cancelar cpf
+		JButton btnCancelarRes_1 = new JButton("Cancelar(CPF)");
+		btnCancelarRes_1.setFont(new Font("Gadugi", Font.PLAIN, 12));
 		btnCancelarRes_1.setEnabled(false);
 		btnCancelarRes_1.setBounds(29, 251, 120, 23);
 		btnCancelarRes_1.setEnabled(false);
@@ -180,10 +180,10 @@ public class ExcursaoInterface {
 		});
 		frameExcursao.getContentPane().add(btnCancelarRes_1);
 		
-		//botão: listar por CPF
+		//botao: listar por CPF
 		JButton btnListarCPF = new JButton("Listar por CPF");
 		btnListarCPF.setBounds(29, 319, 120, 23);
-		btnListarCPF.setFont(new Font("Gadugi", Font.PLAIN, 11));
+		btnListarCPF.setFont(new Font("Gadugi", Font.PLAIN, 12));
 		btnListarCPF.setEnabled(false);
 		btnListarCPF.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -202,23 +202,23 @@ public class ExcursaoInterface {
 		});
 		frameExcursao.getContentPane().add(btnListarCPF);
 
-		//botão: valor total
+		//botao: valor total
 		JButton btnValorTotal = new JButton("Valor total");
-		btnValorTotal.setFont(new Font("Gadugi", Font.PLAIN, 11));
+		btnValorTotal.setFont(new Font("Gadugi", Font.PLAIN, 12));
 		btnValorTotal.setEnabled(false);
 		btnValorTotal.setBounds(29, 136, 120, 23);
 		btnValorTotal.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				txtrAsReservasDa.setText("Valor: R$: "+excursao.calcularValorTotal());
+				txtrAsReservasDa.setText("Valor total: R$: "+excursao.calcularValorTotal());
 			}
 		});
 		frameExcursao.getContentPane().add(btnValorTotal);
 		
-		//botão: salvar
+		//botao: salvar
 		JButton btnSalvar = new JButton("Salvar");
-		btnSalvar.setFont(new Font("Gadugi", Font.PLAIN, 11));
+		btnSalvar.setFont(new Font("Gadugi", Font.PLAIN, 12));
 		btnSalvar.setEnabled(false);
-		btnSalvar.setBounds(182, 392, 89, 23);
+		btnSalvar.setBounds(170, 387, 89, 23);
 		btnSalvar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				excursao.salvarExcursao();
@@ -226,9 +226,9 @@ public class ExcursaoInterface {
 		});
 		frameExcursao.getContentPane().add(btnSalvar);
 	
-		//botão: listar por nome
+		//botao: listar por nome
 		JButton btnListarNome = new JButton("Listar por nome");
-		btnListarNome.setFont(new Font("Gadugi", Font.PLAIN, 11));
+		btnListarNome.setFont(new Font("Gadugi", Font.PLAIN, 12));
 		btnListarNome.setBounds(29, 353, 120, 23);
 		btnListarNome.setEnabled(false);
 		btnListarNome.addActionListener(new ActionListener() {
@@ -249,9 +249,9 @@ public class ExcursaoInterface {
 		});
 		frameExcursao.getContentPane().add(btnListarNome);
 		
-		//botão: criar excursão
+		//botao: criar excursão
 		JButton btnCriarExcursao = new JButton("Criar");
-		btnCriarExcursao.setFont(new Font("Gadugi", Font.PLAIN, 11));
+		btnCriarExcursao.setFont(new Font("Gadugi", Font.PLAIN, 12));
 		btnCriarExcursao.setBounds(29, 102, 120, 23);
 		btnCriarExcursao.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -280,10 +280,10 @@ public class ExcursaoInterface {
 		});
 		frameExcursao.getContentPane().add(btnCriarExcursao);
 		
-		//botão: carregar
+		//botao: carregar
 		JButton btnCarregar = new JButton("Carregar");
-		btnCarregar.setFont(new Font("Gadugi", Font.PLAIN, 11));
-		btnCarregar.setBounds(281, 392, 89, 23);
+		btnCarregar.setFont(new Font("Gadugi", Font.PLAIN, 12));
+		btnCarregar.setBounds(281, 387, 89, 23);
 		btnCarregar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
